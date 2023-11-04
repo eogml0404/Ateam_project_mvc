fd4dd83e02965636027e5dda92f5ed3ff4d01888
ElliDaon <ekdhs2939@naver.com>
2023-11-05 02:09:25 +0900

    1104 commit

--- Ezen_Univ/src/main/webapp/js/index.js
+++ Ezen_Univ/src/main/webapp/js/index.js
@@ -1,4 +1,5 @@
 const wrapper = document.querySelector('.wrapper');
+const first = document.querySelector('.first');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnpopup = document.querySelector('.btnLogin-popup');
@@ -14,8 +15,46 @@ loginLink.addEventListener('click', ()=> {
 
 btnpopup.addEventListener('click', ()=> {
     wrapper.classList.add('active-popup');
+    first.classList.add('active-popup');
 });
 
 closebtn.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
+    first.classList.remove('active-popup');
 });
+
+function login(){
+    var fm = document.frm;
+
+    var value = document.querySelector('input[name="select"]:checked').value;
+    alert(value);
+    if(value==='student'){
+        alert('학생페이지이동');
+        fm.action ="<%=request.getContextPath()%>/member/studentLoginAction.html"; 
+	    fm.method = "post";  
+	    fm.submit();
+        return;
+    }else{
+        alert('교수페이지 이동');
+    }
+
+    var radios = document.getElementsByName('select');
+    /*for(var radio of radios){
+        if(radio.checked){
+            var checked_select = radio.value;
+            // alert(checked_select);
+            if(checked_select === "student"){
+                fm.action ="<%=request.getContextPath()%>/member/memberLoginAction.do";  //처리하기위해 이동하는 주소
+	            fm.method = "post";  //이동하는 방식  get 노출시킴 post 감추어서 전달
+	            fm.submit();
+	            return;
+                location.href='./main/main_s.html';
+                break;
+            }else{
+                location.href='./main/main_p.html';
+                break;
+            }
+        }
+    }
+    */
+}
