fc303af8abfaa5415b6c3c6a73d2c90b779cd57a
ElliDaon <ekdhs2939@naver.com>
2023-11-06 08:45:34 +0900

    dd

--- Ezen_Univ/src/main/java/app/controller/MemberController.java
+++ Ezen_Univ/src/main/java/app/controller/MemberController.java
@@ -1,142 +1,142 @@
-package app.controller;
+// package app.controller;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
+// import java.io.IOException;
+// import java.io.PrintWriter;
+// import java.util.ArrayList;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+// import javax.servlet.RequestDispatcher;
+// import javax.servlet.ServletException;
+// import javax.servlet.annotation.WebServlet;
+// import javax.servlet.http.HttpServlet;
+// import javax.servlet.http.HttpServletRequest;
+// import javax.servlet.http.HttpServletResponse;
+// import javax.servlet.http.HttpSession;
 
-import app.dao.MemberDao;
-import app.domain.MemberVo;
+// import app.dao.MemberDao;
+// import app.domain.MemberVo;
 
-//HttpServlet�� ��ӹ޾ұ� ������ Ŭ������ ���ͳ��������� �ȴ�.
-@WebServlet("/MemberController")
-public class MemberController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+// //HttpServlet�� ��ӹ޾ұ� ������ Ŭ������ ���ͳ��������� �ȴ�.
+// @WebServlet("/MemberController")
+// public class MemberController extends HttpServlet {
+// 	private static final long serialVersionUID = 1L;
 	
-	private String location;
-	public MemberController(String location) {
-		this.location = location;
-	}
+// 	private String location;
+// 	public MemberController(String location) {
+// 		this.location = location;
+// 	}
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+// 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		if(location.equals("memberList.do")) {
+// 		if(location.equals("memberList.do")) {
 			
-			MemberDao md = new MemberDao();
-			ArrayList<MemberVo> list = md.memberSelectAll();
+// 			MemberDao md = new MemberDao();
+// 			ArrayList<MemberVo> list = md.memberSelectAll();
 			
-			request.setAttribute("list", list);
+// 			request.setAttribute("list", list);
 			
-			String path = "/member/memberList.jsp";
-			RequestDispatcher rd = request.getRequestDispatcher(path);
-			rd.forward(request, response);
+// 			String path = "/member/memberList.jsp";
+// 			RequestDispatcher rd = request.getRequestDispatcher(path);
+// 			rd.forward(request, response);
 			
-		} else if(location.equals("memberJoin.do")) {
+// 		} else if(location.equals("memberJoin.do")) {
 			
-			String path = "/member/memberJoin.jsp";
-			RequestDispatcher rd = request.getRequestDispatcher(path);
-			rd.forward(request, response);
+// 			String path = "/member/memberJoin.jsp";
+// 			RequestDispatcher rd = request.getRequestDispatcher(path);
+// 			rd.forward(request, response);
 			
-		} else if(location.equals("memberJoinAction.do")) {
+// 		} else if(location.equals("memberJoinAction.do")) {
 			
-			String memberId = request.getParameter("memberId");
-			String memberName = request.getParameter("memberName");
-			String memberPwd = request.getParameter("memberPwd");
-			String memberPhone = request.getParameter("memberPhone");
-			String memberEmail = request.getParameter("memberEmail");
-			String memberYear = request.getParameter("memberYear");
-			String memberMonth = request.getParameter("memberMonth");
-			String memberDay = request.getParameter("memberDay");
-			String memberGender = request.getParameter("memberGender");
-			String memberAddr = request.getParameter("memberAddr");
-			String[] memberHobby = request.getParameterValues("memberHobby");
-			String str = "";
-			for(int i=0; i<memberHobby.length; i++){
-				str += memberHobby[i];
-				str += ", ";
-			}
-			str = str.substring(0, str.length()-2); 
+// 			String memberId = request.getParameter("memberId");
+// 			String memberName = request.getParameter("memberName");
+// 			String memberPwd = request.getParameter("memberPwd");
+// 			String memberPhone = request.getParameter("memberPhone");
+// 			String memberEmail = request.getParameter("memberEmail");
+// 			String memberYear = request.getParameter("memberYear");
+// 			String memberMonth = request.getParameter("memberMonth");
+// 			String memberDay = request.getParameter("memberDay");
+// 			String memberGender = request.getParameter("memberGender");
+// 			String memberAddr = request.getParameter("memberAddr");
+// 			String[] memberHobby = request.getParameterValues("memberHobby");
+// 			String str = "";
+// 			for(int i=0; i<memberHobby.length; i++){
+// 				str += memberHobby[i];
+// 				str += ", ";
+// 			}
+// 			str = str.substring(0, str.length()-2); 
 
 
-			String memberBirth=memberYear+memberMonth+memberDay;
+// 			String memberBirth=memberYear+memberMonth+memberDay;
 
 
-			//Statement stmt = conn.createStatement();
+// 			//Statement stmt = conn.createStatement();
 
-			 String new_index_q = "SELECT MAX(MIDX) FROM MEMBER0803";
-			ResultSet new_index = stmt.executeQuery(new_index_q);
-			int index_num =0;
-			while(new_index.next()) {
-				index_num = new_index.getInt(1) +1;
-			} 
+// 			 String new_index_q = "SELECT MAX(MIDX) FROM MEMBER0803";
+// 			ResultSet new_index = stmt.executeQuery(new_index_q);
+// 			int index_num =0;
+// 			while(new_index.next()) {
+// 				index_num = new_index.getInt(1) +1;
+// 			} 
 			
-			MemberDao md = new MemberDao();
-			int exec = md.memberInsert(memberId, memberPwd, memberName, memberBirth, memberGender, memberPhone, memberEmail, memberAddr, str);
+// 			MemberDao md = new MemberDao();
+// 			int exec = md.memberInsert(memberId, memberPwd, memberName, memberBirth, memberGender, memberPhone, memberEmail, memberAddr, str);
 
-			PrintWriter out = response.getWriter();
+// 			PrintWriter out = response.getWriter();
 
 
-			//System.out.println(sql);
-			//System.out.println(tf);
-			if(exec == 1){
+// 			//System.out.println(sql);
+// 			//System.out.println(tf);
+// 			if(exec == 1){
 
-				//response.sendRedirect(request.getContextPath()+"/member/memberList.html");
-				out.println("<script>alert(\"ȸ�����ԵǾ����ϴ�.\");"
-				+"document.location.href='"+request.getContextPath()+"/member/memberList.do'</script>");
-			}else{
-				out.println("<script>history.back();</script>");
-			}
+// 				//response.sendRedirect(request.getContextPath()+"/member/memberList.html");
+// 				out.println("<script>alert(\"ȸ�����ԵǾ����ϴ�.\");"
+// 				+"document.location.href='"+request.getContextPath()+"/member/memberList.do'</script>");
+// 			}else{
+// 				out.println("<script>history.back();</script>");
+// 			}
 			
-		}else if(location.equals("memberLogin.do")) {
-			String path = "/member/memberLogin.jsp";
+// 		}else if(location.equals("memberLogin.do")) {
+// 			String path = "/member/memberLogin.jsp";
 
-			RequestDispatcher rd = request.getRequestDispatcher(path);
-			rd.forward(request, response);
-		}else if(location.equals("memberLogout.do")) {
-			HttpSession session = request.getSession();
-			session.removeAttribute("memberId");
-			session.removeAttribute("midx");
-			session.invalidate();
+// 			RequestDispatcher rd = request.getRequestDispatcher(path);
+// 			rd.forward(request, response);
+// 		}else if(location.equals("memberLogout.do")) {
+// 			HttpSession session = request.getSession();
+// 			session.removeAttribute("memberId");
+// 			session.removeAttribute("midx");
+// 			session.invalidate();
 			
-			response.sendRedirect(request.getContextPath()+"/");
+// 			response.sendRedirect(request.getContextPath()+"/");
 			
-		}else if(location.equals("memberLoginAction.do")) {
+// 		}else if(location.equals("memberLoginAction.do")) {
 			
-			String memberId = request.getParameter("memberId");
-			String memberPwd = request.getParameter("memberPwd");
+// 			String memberId = request.getParameter("memberId");
+// 			String memberPwd = request.getParameter("memberPwd");
 			
 			
-			MemberDao md = new MemberDao();
-			int midx = 0;
-			midx = md.memberLoginCheck(memberId, memberPwd);
-			if (midx!=0){ 
-				HttpSession session = request.getSession();
-				session.setAttribute("memberId", memberId);
-				session.setAttribute("midx", midx);
+// 			MemberDao md = new MemberDao();
+// 			int midx = 0;
+// 			midx = md.memberLoginCheck(memberId, memberPwd);
+// 			if (midx!=0){ 
+// 				HttpSession session = request.getSession();
+// 				session.setAttribute("memberId", memberId);
+// 				session.setAttribute("midx", midx);
 				
-				response.sendRedirect(request.getContextPath()+"/index.jsp");
+// 				response.sendRedirect(request.getContextPath()+"/index.jsp");
 				
-			}else{
-				response.setCharacterEncoding("UTF-8");
-				response.setContentType("text/html; charset=UTF-8");
-				PrintWriter out = response.getWriter();
-				out.println("<script language='javascript' type='text/javascript'> alert('�α��� ����'); </script>");
-				out.println("<script>location.href='memberLogin.do';</script>");
-				//response.sendRedirect("./memberLogin.do");
-			}
+// 			}else{
+// 				response.setCharacterEncoding("UTF-8");
+// 				response.setContentType("text/html; charset=UTF-8");
+// 				PrintWriter out = response.getWriter();
+// 				out.println("<script language='javascript' type='text/javascript'> alert('�α��� ����'); </script>");
+// 				out.println("<script>location.href='memberLogin.do';</script>");
+// 				//response.sendRedirect("./memberLogin.do");
+// 			}
 			
-		}
-	}
+// 		}
+// 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-	}
+// 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+// 		doGet(request, response);
+// 	}
 
-}
+// }
---
+++ Ezen_Univ/src/main/webapp/index.jsp
@@ -0,0 +1,123 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+KR:wght@200;300&family=Passion+One:wght@400;700&family=Quicksand&display=swap" rel="stylesheet">
+<link rel="website icon" type="png" href="images/ezen.png">
+<link rel="stylesheet" href="css/index_style.css">
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</head>
+<body>	
+	<header>
+		<h2 class="logo"><a href="index.html">Ezen_Univ</a></h2>
+		<nav class="navigation">
+			<a href="https://www.cubeitac.com/">HOME</a>
+			<a href="#">INFO</a>
+			<button class="btnLogin-popup">SIGN-IN</button>
+		</nav>
+	</header>
+	<div class="first">
+		<h1>Ezen University</h1><br>
+		<p>저희 이젠은 4차 산업혁명을 선도하는 전문교육기관이 되고자 노력하며,<br>
+		훈련생과 훈련생을 채용한 산업체 모두가 만족할 수 있는 기관이 되고자 
+		최선을 다할 것입니다.<br><br></p>
+	</div>
+	<div class="wrapper">
+		<span class="icon-close"><ion-icon name="close"></ion-icon></span>
+		<div class="form-box login">
+			<h2>Login</h2>
+			<form action="#">
+				<div class="input-box">
+					<span class="icon"><ion-icon name="person"></ion-icon></span>
+					<input type="text" required>
+					<label>ID</label>
+				</div>
+				<div class="input-box">
+					<span class="icon"><ion-icon name="key"></ion-icon></span>
+					<input type="password" required>
+					<label>password</label>
+				</div>
+				<div class="remember-forgot">
+					
+					<button onclick="window.open('member/searchInfo.html','window_name','width=500,height=500,location=no,status=no,scrollbars=yes');">ID | password 찾기</button>
+				</div>
+				<div class="choose">
+					<input type="radio" id="select_stu" name="select" value="student" checked><label for="select_stu">student</label> | 
+					<input type="radio" id="select_pro" name="select" value="professor"><label for="select_pro">professor</label>
+				</div>
+				<button type="submit" class="btn" onclick="login()">Login</button>
+				<div class="login-register">
+					<p><a href="#" class="register-link">SIGN-UP</a></p>
+				</div>
+			</form>
+		</div>
+		
+		<div class="form-box register">
+			<h2 style="font-size: 30px;">Registration</h2>
+			<form action="#">
+				<div class="input-box">
+					<span class="icon"><ion-icon name="person"></ion-icon></span>
+					<input type="text" required>
+					<label>ID</label>
+				</div>
+				<div class="input-box">
+					<span class="icon"><ion-icon name="key"></ion-icon></span>
+					<input type="password" required>
+					<label>password</label>
+				</div>
+				<div class="input-box">
+					<span class="icon"><ion-icon name="key"></ion-icon></span>
+					<input type="password" required>
+					<label>password 확인</label>
+				</div>
+				<div class="input-box">
+					<span class="icon"><ion-icon name="pricetag"></ion-icon></span>
+					<input type="text" required>
+					<label>이름<span>(홍길동)</span></label>
+				</div>
+				<div class="input-box">
+					<span class="icon"><ion-icon name="call"></ion-icon></span>
+					<input type="tel" required>
+					<label>연락처<span>(01011112222)</span></label>
+				</div>
+				<div class="input-box">
+					<span class="icon"><ion-icon name="mail"></ion-icon></span>
+					<input type="email" required>
+					<label>이메일<span>(ezen@ezen.com)</span></label>
+				</div>
+				<div class="input-box birth">
+					<span class="icon"><ion-icon name="calendar-number"></ion-icon></span>
+					<input type="text" required>
+					<label>생년월일<span>(19941024)</span></label>
+				</div>
+				<div class="input-box birth">
+					<span class="icon"><ion-icon name="school"></ion-icon></span>
+					<input type="text" list="depList" required>
+					<datalist id="depList" name="country" size="50" autocomplete="off">
+						<option value="정보통신공학과"></option>
+						<option value="전기공학과"></option>
+						<option value="전자공학과"></option>
+						<option value="건축학과"></option>
+					</datalist>
+					<label class="choosemajor">학과</label>
+				</div>
+				<div class="choose">
+					<input type="radio" id="select_stu" name="select" value="student" checked><label for="select_stu">student</label> | 
+					<input type="radio" id="select_pro" name="select" value="professor"><label for="select_pro">professor</label>
+				</div>
+				<button type="submit" class="btn">회원가입</button>
+				<div class="login-register">
+					<p><a href="#" class="login-link">LOGIN</a></p>
+				</div>
+			</form>
+		</div>
+	</div>
+	<script src="js/index.js"></script>
+</body>
+</html>
