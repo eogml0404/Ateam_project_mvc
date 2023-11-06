88f514f1e466d3aa552be27d3d4c6c81d56429a4
ElliDaon <ekdhs2939@naver.com>
2023-11-06 14:04:21 +0900

    dd

--- Ezen_Univ/src/main/webapp/js/attendanceManage.js
+++ Ezen_Univ/src/main/webapp/js/attendanceManage.js
@@ -1,7 +1,17 @@
 for(let i=0; i<=3; i++){
-    function getValue(event){
-        const value=event.target.value;
+    const getValue = "getValue"+i;
 
-        document.getElementById('value').value=value;
+    function getValue(event){
+        
+        const value=event.target.value;
+        const eleid="value"+i;
+        document.getElementById(eleid).value=value;
     }
+}
+
+function selectAtt(selectAll){
+    const checkboxes = document.getElementsByName('student');
+    checkboxes.forEach((checkbox) => {
+        checkbox.checked = selectAll.checked;
+    })
 }
